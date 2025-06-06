c88eaf1a319bd0861a614995409056727160f422
alekseiLazunin <gegcuk@gmail.com>
2025-06-06 12:15:13 +0100

    main page works

--- quizmaker-frontend/src/main.tsx
+++ quizmaker-frontend/src/main.tsx
@@ -3,14 +3,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import AppRoutes from './routes/AppRoutes'; // ⬅️ the file you just added
+import AppRoutes from './routes/AppRoutes';
+import { AuthProvider } from './context/AuthContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* Your AuthProvider should wrap everything that needs useAuth() */}
-      <AppRoutes />
+      <AuthProvider>
+        <AppRoutes />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
